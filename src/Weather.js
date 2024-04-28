3871428c6cd61c2db183612784d4235f8047f3bb
reb84 <152401972+reb84@users.noreply.github.com>
2024-04-28 16:55:23 +0100

    redesigned html/css and added favicon

--- src/Weather.js
+++ src/Weather.js
@@ -4,138 +4,152 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
+    <div className="container">
       <div className="weather-container">
-        <div className="header-search">
-          <form className="search-form" id="search-form">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              required
-              id="search-form-input"
-              className="search-form-input"
-            />
-            <input type="submit" value="Search" className="search-form-button" />
-          </form>
-        </div>
-        <div className="current-main">
-          <div className="current-where">
-            <h1 className="current-city" id="current-city">
-              London
-            </h1>
-          </div>
-          <div className="current-when">
-            <span id="current-date">SUNDAY - 7 APRIL - 10:19</span>
-          </div>
-          <div className="current-temp-container">
+        <div className="cwc-left">
+          <div className="current-media-top">
             <div id="current-weather-icon">
               <img
                 src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-                className="weather-icon"
+                class="current-weather-icon"
               />
             </div>
-            <div className="current-temp-value" id="current-temp">
-              14
+            <div className="current-temp-container">
+              <span className="current-temp-value" id="current-temp">
+                20
+              </span>
+              <span className="current-temp-unit">°C</span>
             </div>
-            <div className="current-temp-unit">°C</div>
+          </div>
+
+          <div className="current-date-container">FRIDAY - 12 APRIL - 09:56</div>
+
+          <div className="current-city">London</div>
+
+          <div className="current-description">
+            <div>
+              <p class="current-details">
+                <span id="description">overcast clouds</span>
+                <br />
+                Humidity: <strong id="humidity">73%</strong>
+                <br />
+                Wind: <strong id="wind-speed">4.92km/h</strong>
+              </p>
+            </div>
+          </div>
+          <div className="temp-switch">
+            <a href="/" span id="switch-cel">
+              C
+            </a>{" "}
+            /{" "}
+            <a href="/" span id="switch-far">
+              F
+            </a>
           </div>
         </div>
 
-        <div className="current-extra">
-          <div>
-            <p className="current-details">
-              <span id="description">broken clouds</span>
-              <br />
-              Humidity: <strong id="humidity">68%</strong>, Wind:
-              <strong id="wind-speed">8.75km/h</strong>
-            </p>
-          </div>
-        </div>
+        <div className="cwc-right">
+          <div className="header-search-container">
+            <form>
+              <div className="search-form" id="search-form">
+                <input
+                  type="search"
+                  class="form-control"
+                  id="search-form-input"
+                  placeholder="Enter a city.."
+                />
 
-        <div className="weather-forecast" id="forecast">
-          <div className="weather-forecast-day">
-            <div className="weather-forecast-date">Mon</div>
-            <img
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
-              className="forecast-icon"
-            />
-            <div className="forecast-temps">
+                <button type="submit" value="Search" class="search-form-button">
+                  submit
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div className="forecast-weather-container">
+            <div className="weather-forecast-day">
+              <div className="icon-container">
+                <img
+                  src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  class="forecast-icon"
+                />
+              </div>
+              <div className="weather-forecast-date">Saturday</div>
               <div className="weather-forecast-temperature">
-                <span className="weather-temp-max">15º</span>/
-                <span className="weather-temp-min">11º</span>
+                <span className="forecast-temps-max">20</span> /
+                <span className="forecast-temps-min">10</span>{" "}
+                <span className="forecast-unit">ºC</span>
               </div>
             </div>
-          </div>
 
-          <div className="weather-forecast-day">
-            <div className="weather-forecast-date">Tue</div>
-            <img
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
-              className="forecast-icon"
-            />
-            <div className="forecast-temps">
+            <div className="weather-forecast-day">
+              <div className="icon-container">
+                <img
+                  src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  class="forecast-icon"
+                />
+              </div>
+              <div className="weather-forecast-date">Sunday</div>
               <div className="weather-forecast-temperature">
-                <span className="weather-temp-max">15º</span>/
-                <span className="weather-temp-min">11º</span>
+                <span className="forecast-temps-max">20</span> /
+                <span className="forecast-temps-min">10</span>{" "}
+                <span className="forecast-unit">ºC</span>
               </div>
             </div>
-          </div>
 
-          <div className="weather-forecast-day">
-            <div className="weather-forecast-date">Wed</div>
-            <img
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
-              className="forecast-icon"
-            />
-            <div className="forecast-temps">
+            <div className="weather-forecast-day">
+              <div className="icon-container">
+                <img
+                  src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  class="forecast-icon"
+                />
+              </div>
+              <div className="weather-forecast-date">Monday</div>
               <div className="weather-forecast-temperature">
-                <span className="weather-temp-max">15º</span>/
-                <span className="weather-temp-min">11º</span>
+                <span className="forecast-temps-max">20</span> /
+                <span className="forecast-temps-min">10</span>{" "}
+                <span className="forecast-unit">ºC</span>
               </div>
             </div>
-          </div>
 
-          <div className="weather-forecast-day">
-            <div className="weather-forecast-date">Thu</div>
-            <img
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
-              className="forecast-icon"
-            />
-            <div className="forecast-temps">
+            <div className="weather-forecast-day">
+              <div className="icon-container">
+                <img
+                  src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  class="forecast-icon"
+                />
+              </div>
+              <div className="weather-forecast-date">Tuesday</div>
               <div className="weather-forecast-temperature">
-                <span className="weather-temp-max">15º</span>/
-                <span className="weather-temp-min">11º</span>
+                <span className="forecast-temps-max">20</span> /
+                <span className="forecast-temps-min">10</span>{" "}
+                <span className="forecast-unit">ºC</span>
               </div>
             </div>
-          </div>
 
-          <div className="weather-forecast-day">
-            <div className="weather-forecast-date">Fri</div>
-            <img
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
-              className="forecast-icon"
-            />
-            <div className="forecast-temps">
+            <div className="weather-forecast-day">
+              <div className="icon-container">
+                <img
+                  src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  class="forecast-icon"
+                />
+              </div>
+              <div className="weather-forecast-date">Wednesday</div>
               <div className="weather-forecast-temperature">
-                <span className="weather-temp-max">15º</span>/
-                <span className="weather-temp-min">11º</span>
+                <span className="forecast-temps-max">20</span> /
+                <span className="forecast-temps-min">10</span>{" "}
+                <span className="forecast-unit">ºC</span>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="footer-element">
-        <p>
-          <a href="https://github.com/reb84/weather-app-react" target="_blank">
-            Open-source code
-          </a>{" "}
-          by Rebecca Upton. Hosted{" "}
-          <a href="https://weather-app-react-zeta-flame.vercel.app/" target="_blank">
-            on Vercel
-          </a>
-          .
-        </p>
+      <div className="footer-container">
+        Project coded by{" "}
+        <a href="https://www.shecodes.io/graduates/97811-rebecca-upton">Rebecca Upton</a>, is{" "}
+        <a href="https://github.com/reb84/weather-app-react">open-sourced</a> and hosted on{" "}
+        <a href="https://weather-app-react-zeta-flame.vercel.app/">Vercel</a>
       </div>
     </div>
   );
